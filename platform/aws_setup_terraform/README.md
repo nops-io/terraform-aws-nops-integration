926974af8523c467e0ed82b3f1a264c0b337bc25
Le Kien Truc <truc@nops.io>
2024-08-13 21:55:31 +0700

    Add platform onboarding using terrform

---
+++ platform/aws_setup_terraform/README.md
@@ -0,0 +1,52 @@
+# nOps AWS Integration Terraform Module
+
+## Description
+
+This Terraform module automates the process of integrating your AWS account with nOps, a cloud management and optimization platform. It streamlines the setup of necessary AWS resources and permissions, enhancing the onboarding experience for nOps users.
+
+## Features
+
+- Automatic detection of existing nOps projects for the AWS account
+- Creation of new nOps projects if none exist
+- Handling of master and member AWS accounts
+- Automatic setup of IAM roles and policies for nOps integration
+- S3 bucket creation and configuration for master accounts
+- Integration with nOps API for secure token exchange
+
+## Prerequisites
+
+- Terraform v0.13+
+- AWS CLI configured with appropriate permissions
+- nOps API key
+
+## Usage
+
+1. Clone this repository:
+
+
+2. Create a `terraform.tfvars` file with your specific variables:
+```hcl
+aws_region = "us-west-2"
+api_key    = "your-nops-api-key"
+system_bucket_id = "your-system-bucket-id"
+```
+
+3. Initialize Terraform:
+
+terraform init
+
+Plan and apply the Terraform configuration:
+
+
+4. Troubleshooting
+
+If you want to reinstall the stack you might got problem like
+
+```
+│ Error: creating IAM Role (NopsIntegrationRole-xxxxx): EntityAlreadyExists: Role with name NopsIntegrationRole-xxxxx already exists.
+```
+
+You can import the role to terraform state by running the following command
+```
+terraform import aws_iam_role.nops_integration_role NopsIntegrationRole-xxxxx
+```
---
+++ platform/aws_setup_terraform/iam.tf
@@ -0,0 +1,303 @@
+# iam.tf
+
+resource "null_resource" "force_new_role" {
+  triggers = {
+    account_id = local.account_id
+  }
+}
+
+
+resource "aws_iam_role" "nops_integration_role" {
+  name = "NopsIntegrationRole-${local.client_id}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${var.nops_principal}:root"
+        }
+        Action = "sts:AssumeRole"
+        Condition = {
+          StringEquals = {
+            "sts:ExternalId" = local.external_id
+          }
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+
+  tags = {
+    version   = "1.1.0"
+    createdat = formatdate("YYYYMMDD", timestamp())
+  }
+  depends_on = [null_resource.force_new_role]
+  lifecycle {
+    create_before_destroy = true
+    replace_triggered_by = [null_resource.reconfigure_trigger.id]
+  }
+}
+
+resource "aws_iam_role_policy" "nops_integration_policy" {
+  name = "NopsIntegrationPolicy"
+  role = aws_iam_role.nops_integration_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "cur:DescribeReportDefinitions",
+          "cur:DeleteReportDefinition",
+          "cur:PutReportDefinition",
+          "ce:ListCostAllocationTags",
+          "ce:UpdateCostAllocationTagsStatus",
+          "organizations:InviteAccountToOrganization",
+          "s3:ListBucket",
+          "support:DescribeTrustedAdvisorCheckRefreshStatuses",
+          "support:DescribeTrustedAdvisorCheckResult",
+          "support:DescribeTrustedAdvisorChecks",
+          "wellarchitected:*",
+          "ce:*"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Deny"
+        Action   = [
+          "acm-pca:Describe*",
+          "acm-pca:Get*",
+          "acm-pca:List*",
+          "acm:Describe*",
+          "acm:Get*",
+          "acm:List*",
+          "apigateway:GET",
+          "appconfig:GetConfiguration*",
+          "appflow:DescribeConnector*",
+          "appflow:ListConnector*",
+          "appstream:DescribeDirectoryConfigs",
+          "appstream:DescribeUsers",
+          "appstream:DescribeSessions",
+          "appsync:Get*",
+          "appsync:List*",
+          "athena:Get*",
+          "athena:List*",
+          "backup:GetBackupVaultAccessPolicy",
+          "cassandra:Select",
+          "chime:Describe*",
+          "chime:Get*",
+          "chime:List*",
+          "cloud9:Describe*",
+          "cloud9:Get*",
+          "cloud9:List*",
+          "clouddirectory:Get*",
+          "clouddirectory:List*",
+          "cloudfront:GetCloudFrontOriginAccessIdentity",
+          "cloudfront:GetFieldLevelEncryption*",
+          "cloudfront:GetKeyGroupConfig",
+          "cloudwatch:GetMetricData",
+          "cloudwatch:GetMetricStream",
+          "cloudwatch:ListMetricStreams",
+          "codeartifact:GetAuthorizationToken",
+          "codeartifact:ReadFromRepository",
+          "codebuild:BatchGet*",
+          "codebuild:ListSourceCredentials",
+          "codecommit:BatchGet*",
+          "codecommit:Get*",
+          "codecommit:GitPull",
+          "codedeploy:BatchGet*",
+          "codedeploy:Get*",
+          "codestar:DescribeUserProfile",
+          "codestar:ListUserProfiles",
+          "cognito-identity:*",
+          "cognito-idp:*",
+          "cognito-sync:*",
+          "comprehend:Describe*",
+          "comprehend:List*",
+          "config:BatchGetAggregateResourceConfig",
+          "config:BatchGetResourceConfig",
+          "config:SelectAggregateResourceConfig",
+          "config:SelectResourceConfig",
+          "connect:Describe*",
+          "connect:Get*",
+          "connect:List*",
+          "datapipeline:DescribeObjects",
+          "datapipeline:EvaluateExpression",
+          "datapipeline:QueryObjects",
+          "dax:BatchGetItem",
+          "dax:GetItem",
+          "dax:Query",
+          "deepcomposer:Get*",
+          "deepcomposer:List*",
+          "devicefarm:GetRemoteAccessSession",
+          "devicefarm:ListRemoteAccessSessions",
+          "directconnect:Describe*",
+          "directconnect:List*",
+          "discovery:Describe*",
+          "discovery:Get*",
+          "discovery:List*",
+          "dms:Describe*",
+          "dms:List*",
+          "ds:Get*",
+          "dynamodb:GetItem",
+          "dynamodb:BatchGetItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "ec2:GetConsoleScreenshot",
+          "ecr:BatchGetImage",
+          "ecr:GetAuthorizationToken",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr-public:GetAuthorizationToken",
+          "eks:DescribeIdentityProviderConfig",
+          "elasticbeanstalk:DescribeConfigurationOptions",
+          "elasticbeanstalk:DescribeConfigurationSettings",
+          "es:ESHttpGet*",
+          "fis:GetExperimentTemplate",
+          "fms:GetAdminAccount",
+          "frauddetector:BatchGetVariable",
+          "frauddetector:Get*",
+          "gamelift:GetGameSessionLogUrl",
+          "gamelift:GetInstanceAccess",
+          "geo:ListDevicePositions",
+          "glue:GetSecurityConfiguration*",
+          "glue:SearchTables",
+          "glue:GetTable*",
+          "guardduty:GetIPSet",
+          "guardduty:GetMasterAccount",
+          "guardduty:GetMembers",
+          "guardduty:ListMembers",
+          "guardduty:ListOrganizationAdminAccounts",
+          "inspector2:GetConfiguration",
+          "imagebuilder:GetImage",
+          "iotroborunner:Get*",
+          "iotsitewise:ListAccessPolicies",
+          "ivs:GetPlaybackKeyPair",
+          "ivs:GetStreamSession",
+          "kafka:GetBootstrapBrokers",
+          "kendra:Query*",
+          "kinesis:Get*",
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "lex:Get*",
+          "lambda:GetFunctionConfiguration",
+          "license-manager:GetGrant",
+          "license-manager:GetLicense",
+          "license-manager:ListTokens",
+          "lightsail:GetBucketAccessKeys",
+          "lightsail:GetCertificates",
+          "lightsail:GetContainerImages",
+          "lightsail:GetKeyPair",
+          "lightsail:GetRelationalDatabaseLogStreams",
+          "logs:GetLogEvents",
+          "logs:StartQuery",
+          "machinelearning:GetMLModel",
+          "macie2:GetAdministratorAccount",
+          "macie2:GetMember",
+          "macie2:GetMacieSession",
+          "macie2:SearchResources",
+          "macie2:GetSensitiveDataOccurrences",
+          "nimble:GetStreamingSession",
+          "polly:SynthesizeSpeech",
+          "proton:GetEnvironmentTemplate",
+          "proton:GetServiceTemplate",
+          "proton:ListServiceTemplates",
+          "proton:ListEnvironmentTemplates",
+          "qldb:GetBlock",
+          "qldb:GetDigest",
+          "rds:Download*",
+          "rekognition:CompareFaces",
+          "rekognition:Detect*",
+          "rekognition:Search*",
+          "resiliencehub:DescribeAppVersionTemplate",
+          "resiliencehub:ListRecommendationTemplates",
+          "robomaker:GetWorldTemplateBody",
+          "s3-object-lambda:GetObject",
+          "sagemaker:Search",
+          "schemas:GetDiscoveredSchema",
+          "sdb:Get*",
+          "sdb:Select*",
+          "secretsmanager:*",
+          "securityhub:GetFindings",
+          "securityhub:GetMembers",
+          "securityhub:ListMembers",
+          "ses:GetTemplate",
+          "ses:GetEmailTemplate",
+          "ses:GetContact",
+          "ses:GetContactList",
+          "ses:ListTemplates",
+          "ses:ListEmailTemplates",
+          "ses:ListVerifiedEmailAddresses",
+          "signer:GetSigningProfile",
+          "signer:ListProfilePermissions",
+          "signer:ListSigningProfiles",
+          "sms-voice:DescribeKeywords",
+          "sms-voice:DescribeOptedOutNumbers",
+          "sms-voice:DescribePhoneNumbers",
+          "sms-voice:DescribePools",
+          "snowball:Describe*",
+          "sqs:Receive*",
+          "ssm-contacts:*",
+          "ssm:DescribeParameters*",
+          "ssm:GetParameter*",
+          "s3:Get*",
+          "sso:Describe*",
+          "sso:Get*",
+          "sso:List*",
+          "storagegateway:DescribeChapCredentials",
+          "support:DescribeCommunications",
+          "timestream:ListDatabases",
+          "timestream:ListTables",
+          "transcribe:Get*",
+          "transcribe:List*",
+          "transfer:Describe*",
+          "transfer:List*",
+          "waf-regional:GetChangeToken",
+          "workmail:DescribeUser",
+          "workmail:GetMailUserDetails",
+          "workmail:ListUsers"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "nops_eventbridge_integration_policy" {
+  name = "NopsEventBridgeIntegrationPolicy"
+  role = aws_iam_role.nops_integration_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["events:CreateEventBus"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "nops_system_bucket_policy" {
+  count  = local.is_master_account && var.system_bucket_name != "na" ? 1 : 0
+  name = "NopsSystemBucketPolicy"
+  role = aws_iam_role.nops_integration_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["s3:*"]
+        Resource = [
+          "arn:aws:s3:::${local.bucket_name}",
+          "arn:aws:s3:::${local.bucket_name}/*"
+        ]
+      }
+    ]
+  })
+}
