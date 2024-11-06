eddc5fb1ad704392c1b9488935fdc5c83d724bfe
Werner Matias Kahnlein <matias.kahnlein@caylent.com>
2024-11-06 11:57:00 -0300

    fix: adding nops project as dependency for wait

--- main.tf
+++ main.tf
@@ -24,7 +24,8 @@ resource "time_sleep" "wait_for_resources" {
     aws_iam_role_policy.nops_wafr_policy,
     aws_s3_bucket.nops_system_bucket,
     aws_s3_bucket_policy.nops_bucket_policy,
-    aws_s3_bucket_server_side_encryption_configuration.nops_bucket_encryption
+    aws_s3_bucket_server_side_encryption_configuration.nops_bucket_encryption,
+    nops_project.project
   ]
 
   create_duration = "10s"
