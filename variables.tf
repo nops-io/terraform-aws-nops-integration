22369d5a6441c5e57e72404c1b428f3fee75e65e
Werner Matias Kahnlein <matias.kahnlein@caylent.com>
2024-11-12 13:15:11 -0300

    fix: removing bucket name ver references

--- variables.tf
+++ variables.tf
@@ -12,10 +12,11 @@ variable "min_required_permissions" {
   description = "If true, IAM policies with the min base permissions for nOps to get cost and usage data will be created. Some platform features will not be available."
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "system_bucket_name" {
   type        = string
   default     = "na"
-  description = "The name of the system bucket for nOps integration, this will be deprecated in the future. Keeping for backwards compatibility."
+  description = "[DEPRECATED]  The name of the system bucket for nOps integration."
 }
 
 # tflint-ignore: terraform_unused_declarations
